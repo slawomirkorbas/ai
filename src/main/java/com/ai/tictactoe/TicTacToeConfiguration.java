c06b2470a504bf4c937bc2ca8f3c8c5b055a0075
slawomirkorbas <slawomirkorbas@gmail.com>
2021-02-18 15:15:45 +0100

    Update TicTacToeConfiguration.java
    chnge to net-18-15-12-9-20210120-0840-batch-size-630-epochs-1418.ann

--- src/main/java/com/ai/tictactoe/TicTacToeConfiguration.java
+++ src/main/java/com/ai/tictactoe/TicTacToeConfiguration.java
@@ -27,8 +27,8 @@ public class TicTacToeConfiguration implements WebMvcConfigurer
     AnnTicTacToeAgent annTicTacToeAgent()
     {
         AnnTicTacToeAgent ann =  new AnnTicTacToeAgent("x");
-        //ann.init("net-18-15-12-9-20210120-0840-batch-size-630-epochs-1418.ann");
-        ann.init("net-27-36-9-20210119-0853-batch-size-630-epochs-1353.ann");
+        ann.init("net-18-15-12-9-20210120-0840-batch-size-630-epochs-1418.ann");
+        //ann.init("net-27-36-9-20210119-0853-batch-size-630-epochs-1353.ann");
         return ann;
     }
 
